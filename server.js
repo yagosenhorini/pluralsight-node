743b70b089aa45653114e758d9651a549633367e
yagosenhorini <yagosenhorini30@gmail.com>
2018-05-24 23:38:13 -0300

    Create views directory and configure path and morgan on server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var chalk = require('chalk');
 var debug = require('debug')('app');
+var morgan = require('morgan');
+var path = require('path');
 
 var app = express();
 
+app.use(morgan('combined'));
+
 app.get('/', (req, res)=>{
-    res.send('Hello from my library app');
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 port = process.env.PORT || 3000;
