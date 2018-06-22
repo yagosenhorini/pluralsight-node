09c97fd5bfb2004b1b4cf5f4a22e0b345f3cf272
yagosenhorini <yagosenhorini30@gmail.com>
2018-06-21 23:40:16 -0300

    Add new view file and create router to books page

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 const app = express();
 let port = process.env.PORT || 3000;
+const bookRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
@@ -13,8 +14,43 @@ app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dis
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
 
+const books = [{
+  title: 'War and Peace',
+  genre: 'Historical Fiction',
+  author: 'Lev Nikolayevich Tolstoy',
+  read: false
+},
+]
+
+
+bookRouter.route('/')
+.get((req, res)=>{
+  res.render(
+    'books',
+    {
+    title: 'Books', 
+    nav: [
+      {link: '/books', title: 'Books'},
+      {link: '/authors', title:'Authors'}]
+    }) 
+});
+
+bookRouter.route('/single')
+.get((req, res)=>{
+  res.send('Hello single book');
+});
+
+app.use('/books', bookRouter);
+
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Node Library App', list: ['a','b'] });
+  res.render(
+    'index', 
+    { 
+      title: 'Node Library App', 
+      nav: [{link: '/books', title: 'Books'},
+      {link: '/authors', title:'Authors'}] 
+    }
+  );
 });
 
 app.set('views', './src/views');
